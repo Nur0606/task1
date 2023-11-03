07ce1db33d4ade880083a5452f0d7b57c6d0b4a2
nurhan <141030168+Nur0606@users.noreply.github.com>
2023-11-03 19:30:56 +0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+Rectengl r = new Rectengl(20,20);
+         System.out.println("dlina = " + r.getDlina());
+         System.out.println("shirina = " + r.getDlina());
+Rectengl rectengl = new Rectengl();
+rectengl.setDlina(10);
+rectengl.setShirina(5);
+
+
+    }
+}
---
+++ src/Rectengl.java
@@ -0,0 +1,28 @@
+public class Rectengl {
+    private double dlina;
+    private double shirina;
+
+public Rectengl(){
+
+}
+    public Rectengl(double dlina, double shirina) {
+        this.dlina = dlina;
+        this.shirina = shirina;
+    }
+
+    public double getDlina() {
+        return dlina;
+    }
+
+    public void setDlina(double dlina) {
+        this.dlina = dlina;
+    }
+
+    public double getShirina() {
+        return shirina;
+    }
+
+    public void setShirina(double shirina) {
+        this.shirina = shirina;
+    }
+}
